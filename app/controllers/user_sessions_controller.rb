f20047a06838d3a85dbbd193bafab7aa52db4a9d
Henrique Testa <hgtesta@gmail.com>
2010-05-03 19:03:12 -0300

    Login working

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,8 +5,6 @@ class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
   
-  layout "login"
-  
   def new
     @user_session = UserSession.new
   end
@@ -16,8 +14,8 @@ class UserSessionsController < ApplicationController
     if @user_session.save
       flash[:success] = "Login successful!"
       @user = User.find_by_login(@user_session.login)  # can't use current_user here yet
-      AppLogger.info "user '#{@user.login}' succefully logged in"
-      redirect_to @user.admin? ? admin_presentations_path : visits_path
+      # AppLogger.info "user '#{@user.login}' succefully logged in"
+      redirect_to root_path
     else
       AppLogger.warn "Log in failed for '#{params[:user_session][:login]}'"
       render :action => :new
