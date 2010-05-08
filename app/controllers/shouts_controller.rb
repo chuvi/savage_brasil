e4446af8bed4a45dca9eb0b9fdb68064af563d80
Henrique Testa <hgtesta@gmail.com>
2010-05-08 15:43:37 -0300

    Add content limit to shout

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -3,8 +3,12 @@ class ShoutsController < ApplicationController
   def create
     @shout = Shout.new(params[:shout])
     @shout.user = current_user
-    @shout.save
-    redirect_to root_path
+    if @shout.save
+      redirect_to root_path
+    else
+      @shouts = Shout.all :limit => 5, :order => "created_at DESC"
+      render "home/index"
+    end
   end
   
 end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,4 +2,6 @@ class Shout < ActiveRecord::Base
   
   belongs_to :user
   
+  validates_length_of :content, :maximum => 300
+  
 end
