84db7a559b600d602ade1fefd48d8af52e1859de
Henrique Testa <hgtesta@gmail.com>
2010-05-19 01:50:37 -0300

    Fixed width & height for image boxes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
   validates_presence_of :name
   
   has_many :shouts
-  has_many :assets
+  has_many :assets # necessary?
+  has_many :demos
+  has_many :images
   
   # the configuration block is optional, see authlogic docs for more configurations
   acts_as_authentic do |c|
