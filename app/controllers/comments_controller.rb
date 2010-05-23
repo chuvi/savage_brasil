ef52b743f0f0f012a5adc4876b806d9f04c7dafe
Henrique Testa <hgtesta@gmail.com>
2010-05-22 23:23:21 -0300

    Event for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,15 @@
 class CommentsController < ApplicationController
   
-  # def new
-  #   @comment = Comment.new
-  # end
-  
   def create
-    klass = params[:comment][:commentable_type].classify.constantize
+    type = params[:comment][:commentable_type]
+    klass = type.classify.constantize
     @parent = klass.find(params[:parent_id])
-    # @comment = Comment.new(params[:comment])
     @comment = @parent.comments.build params[:comment]
     @comment.user = current_user
     if @parent.save
       flash[:success] = "Comentário adicionado"
       flash[:comment_created] = true
+      Event.create :kind => "create_#{type}_comment", type.to_sym => @parent, :user => current_user
     else
       flash[:error] = "Não foi possível adicionar o comentário"
     end
