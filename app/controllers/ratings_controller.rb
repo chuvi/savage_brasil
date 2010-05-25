09fdc8eb12b800eb4e8503f2ade34d88373a9101
Henrique Testa <hgtesta@gmail.com>
2010-05-25 12:47:51 -0300

    Prevent rating abusing

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,18 +2,20 @@ class RatingsController < ApplicationController
   
   def create
     @map = Map.find(params[:map_id])
-    @rating = @map.ratings.build params[:rating]
-    @rating.user = current_user
-    case params[:commit]
-    when "Ruim"
-      @rating.vote = 0
-    when "Indiferente"
-      @rating.vote = 1
-    when "Bom"
-      @rating.vote = 2
-    end
-    if @rating.save!
-      flash[:rating] = "Seu voto foi registrado!"
+    if !@map.ratings.map(&:user_id).include? current_user.id   # prevents duplicated ratings, move this to validations?
+      @rating = @map.ratings.build params[:rating]
+      @rating.user = current_user
+      case params[:commit]
+      when "Ruim"
+        @rating.vote = 0
+      when "Indiferente"
+        @rating.vote = 1
+      when "Bom"
+        @rating.vote = 2
+      end
+      if @rating.save!
+        flash[:rating] = "Seu voto foi registrado!"
+      end
     end
     redirect_to map_path(@map)
   end
