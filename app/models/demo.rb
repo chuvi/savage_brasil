4e83cdcfdf87bf054d38f897beeb86caa3a84ab6
Henrique Testa <hgtesta@gmail.com>
2010-05-29 16:45:26 -0300

    Demo config

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -3,9 +3,20 @@ class Demo < Asset
   validates_presence_of :title
   
   has_many :comments, :as => :commentable
-  
+
   has_attached_file :data
                     # :styles => { :thumb => "200x200>", :medium => "500x500>" },
                     # :default_url => "/images/missing_logo.png"
+  def content
+    @content ||= IO.read(data.path)
+  end
+
+  def config
+    @config ||= content.split("\217\002\000\000").second.split("\003\000\000\000").first.to_savage_hash
+  end
+  
+  def name
+    config["svr_name"]
+  end
   
 end
